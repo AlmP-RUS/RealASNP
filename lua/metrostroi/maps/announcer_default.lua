593e5a18f6d886ae8663e9e93241ba4bab4dbdf5
AlmP-RUS <m.a.uhankov@gmail.com>
2021-07-13 17:43:23 +0300

    Added the "No landing" template

---
+++ lua/metrostroi/maps/announcer_default.lua
@@ -0,0 +1,7 @@
+Metrostroi.AddLastStationTex("717", "posadki_net", "metrostroi_skins/general/posadki_net")
+
+Metrostroi.AddLastStationTex("720", "posadki_net", "metrostroi_skins/general/posadki_net")
+
+--Metrostroi.AddLastStationTex("710", "posadki_net", "metrostroi_skins/general/posadki_net")
+
+--Metrostroi.AddLastStationTex("702", "posadki_net", "metrostroi_skins/general/posadki_net")
